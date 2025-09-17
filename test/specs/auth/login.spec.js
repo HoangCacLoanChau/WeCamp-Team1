cd06b4dd369f2dd7cc86ddc5aa1d95bd30a3a5c4
HuongTran <huongtran.txh@gmail.com>
2025-09-17 23:33:05 +0700

    [Huong] auto ui register

--- test/specs/auth/login.spec.js
+++ test/specs/auth/login.spec.js
@@ -1,17 +1,17 @@
 import LoginPage from "../../pageobjects/auth/login.page";
 
-// specs/login.spec.js
 describe("Login functionality", () => {
+  beforeEach(async () => {
+    await LoginPage.open();
+  });
   afterEach(async () => {
     await browser.reloadSession();
   });
   it("should login with valid credentials", async () => {
-    await LoginPage.open();
     await LoginPage.login("admin@email.com", "123456");
     await expect(browser).toHaveUrl("http://localhost:3000/");
   });
   it("should show error for invalid credentials", async () => {
-    await LoginPage.open();
     await LoginPage.login("wrong@email.com", "wrongpass");
     // Check error message is displayed
     await expect(LoginPage.toast).toHaveText("Invalid email or password");
